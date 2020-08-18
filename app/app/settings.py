7a876fdefb204d0565174b169db54aa93daf126f
Scott Singer <ssinger303@gmail.com>
2020-08-17 20:24:46 -0600

    Converted DB from sqlite3 to Postgresql DB. Everything still works fine

--- app/app/settings.py
+++ app/app/settings.py
@@ -100,10 +100,22 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'laudible_db',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+# The below code worked and was part of this projects standard sqlite3 database syntax
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
--- app/tracks/models.py
+++ app/tracks/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 
 
 class Track(models.Model):
-  title = models.CharField(max_length=50)
+  title = models.CharField(max_length=5000)
   description = models.TextField(blank=True)
   url = models.URLField()
   created_at = models.DateTimeField(auto_now_add=True)
