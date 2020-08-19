b9e16b21bb0ef050cc485563809db31a50064e7a
Scott Singer <ssinger303@gmail.com>
2020-08-19 12:44:12 -0600

    heroku installations

--- app/app/settings.py
+++ app/app/settings.py
@@ -15,18 +15,26 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vfqm&!unt$c04js*cd3k662#3myl+iij6!f*43gv=v31oe$3$k'
+# SECRET_KEY = 'vfqm&!unt$c04js*cd3k662#3myl+iij6!f*43gv=v31oe$3$k'
+
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'vfqm&!unt$c04js*cd3k662#3myl+iij6!f*43gv=v31oe$3$k')
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
+
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
+# export DJANGO_DEBUG = False
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -49,11 +57,11 @@ GRAPHENE = {
     ]
 }
 
-
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -72,7 +80,6 @@ AUTHENTICATION_BACKENDS = [
     'graphql_jwt.backends.JSONWebTokenBackend',
     'django.contrib.auth.backends.ModelBackend',
 ]
-
 
 ROOT_URLCONF = 'app.urls'
 
@@ -150,8 +157,22 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
