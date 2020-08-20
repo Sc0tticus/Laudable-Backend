1b49dc43aa6a25a9cf4790ae1a01142603f28875
Scott Singer <ssinger303@gmail.com>
2020-08-20 15:03:40 -0600

    Added and filled out ReadMe

--- app/settings.py
+++ app/settings.py
@@ -123,15 +123,6 @@ DATABASES = {
     }
 }
 
-# The below code worked and was part of this projects standard sqlite3 database syntax
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
